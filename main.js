9fa2d7fca8ef3d4fc14019f4472ccfd7e701c1c6
MUNROE <319681@dadeschools.net>
2019-04-16 12:01:06 -0400

    Finished html; started on js

---
+++ main.js
@@ -0,0 +1,22 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30;
+
+// Question Set
+var questions = [
+   
+    {
+        question: "What was the first full length CGI movie?",
+        answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
+        correctAnswer: "Toy Story",
+        image: "assets\Toy Story 2..gif"
+    },
+
+    {
+        question: "Which of these is NOT a anme of the Spice Girls?",
+        answers: ["Sporty Spice", "Hot Spice", "Scary Spice", "Posh Spice"],
+        correctAnswer: "Hot Spice",
+        image: "assets\spice girls 2.gif"
+    },
+
+]
